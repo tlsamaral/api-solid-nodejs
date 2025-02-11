f700e29d02410a4f52db62882185146fcc432a95
talles.amaral <worktalles@hotmail.com>
2025-02-11 17:27:05 -0300

    feat: add fastify jwt

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,33 @@
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
-import { appRouter } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
+import { appRouter } from './http/routes'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+	secret: env.JWT_SECRET,
+	// cookie: {
+	// 	cookieName: 'refreshToken',
+	// 	signed: false,
+	// },
+})
 
 app.register(appRouter)
 
 app.setErrorHandler((error, _, reply) => {
-    if (error instanceof ZodError) {
-        return reply
-            .status(400)
-            .send({ message: 'Validation error', issues: error.format() })
-    }
+	if (error instanceof ZodError) {
+		return reply
+			.status(400)
+			.send({ message: 'Validation error', issues: error.format() })
+	}
 
-    if (env.NODE_ENV !== 'production') {
-        console.error(error)
-    } else {
-        // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
-    }
+	if (env.NODE_ENV !== 'production') {
+		console.error(error)
+	} else {
+		// TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+	}
 
-    return reply
-        .status(500)
-        .send({ message: 'Internal server error' })
+	return reply.status(500).send({ message: 'Internal server error' })
 })
