5e8dc02dfb7852c9b42d6dff499639632de919a1
talles.amaral <worktalles@hotmail.com>
2025-02-13 17:28:03 -0300

    feat: create and delete database while test e2e is running

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,41 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+
+import { randomUUID } from 'node:crypto'
 import type { Environment } from 'vitest'
+
+function generateDatabaseURL(schema: string) {
+	if (!process.env.DATABASE_URL) {
+		throw new Error('Please provide a DATABASE_URL environment variable.')
+	}
+
+	const url = new URL(process.env.DATABASE_URL)
+	url.searchParams.set('schema', schema)
+
+	return url.toString()
+}
+
+const prisma = new PrismaClient()
 
 export default (<Environment>{
 	name: 'prisma',
 	transformMode: 'ssr',
 	async setup() {
-		console.log('executou setup')
+		const schema = randomUUID()
+		const databaseURL = generateDatabaseURL(schema)
+
+		process.env.DATABASE_URL = databaseURL
+
+		execSync('npx prisma migrate deploy')
 
 		return {
-			teardown() {},
+			async teardown() {
+				await prisma.$executeRawUnsafe(
+					`DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+				)
+				await prisma.$disconnect()
+			},
 		}
 	},
 })
