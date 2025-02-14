c178c1aaec704aa505ab874ee7ae7d5d3786b349
talles.amaral <worktalles@hotmail.com>
2025-02-14 17:34:05 -0300

    feat: check ins use case

--- src/use-cases/factories/make-check-in-use-case.ts
+++ src/use-cases/factories/make-check-in-use-case.ts
@@ -2,7 +2,7 @@ import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins
 import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
 import { CheckInUseCase } from '../check-in'
 
-export function makeGetUserProfileUseCase() {
+export function makeCheckInUseCase() {
 	const checkInsRepository = new PrismaCheckInsRepository()
 	const gymsRepository = new PrismaGymsRepository()
 	const useCase = new CheckInUseCase(checkInsRepository, gymsRepository)
