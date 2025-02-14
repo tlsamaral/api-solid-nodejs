480efecbbca9a8b73be9bbf297b0bbfef35217ae
talles.amaral <worktalles@hotmail.com>
2025-02-14 17:31:58 -0300

    feat: add fastify cookies

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,30 @@
+import fastifyCookie from '@fastify/cookie'
 import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
-import { appRouter } from './http/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
+import { gymsRoutes } from './http/controllers/gyms/routes'
+import { usersRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
 	secret: env.JWT_SECRET,
-	// cookie: {
-	// 	cookieName: 'refreshToken',
-	// 	signed: false,
-	// },
+	cookie: {
+		cookieName: 'refreshToken',
+		signed: false,
+	},
+	sign: {
+		expiresIn: '10m',
+	},
 })
 
-app.register(appRouter)
+app.register(fastifyCookie)
+
+app.register(usersRoutes)
+app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
 	if (error instanceof ZodError) {
